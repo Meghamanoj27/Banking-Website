816ec7be7a78b988de34cebce7d27ec135ba3953
Meghamanoj27 <124580187+Meghamanoj27@users.noreply.github.com>
2023-12-16 15:22:59 +0530

    Update views.py

--- bank_app/views.py
+++ bank_app/views.py
@@ -1,30 +1,4 @@
-# from django.http import JsonResponse
-# from django.shortcuts import render, redirect
-# from .models import District, Branch, Material
-# from .forms import AccountForm
-# # Create your views here.
 
-#
-#
-#
-# def account_registration(request):
-#     districts = District.objects.all()
-#     materials = Material.objects.all()
-#
-#     if request.method == 'POST':
-#         form = AccountForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('success')  # Redirect to a success page
-#     else:
-#         form = AccountForm()
-#
-#     return render(request, 'registration_form.html', {'form': form, 'districts': districts, 'materials': materials})
-#
-# def get_branches(request):
-#     district_id = request.GET.get('district_id')
-#     branches = Branch.objects.filter(district_id=district_id).values('id', 'name')
-#     return JsonResponse(list(branches), safe=False)
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import District, Branch, Material
